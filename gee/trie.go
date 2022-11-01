fc38aead9bd2494524c4eeb43fcaa24724b9829c
Lawliet <1131927131@qq.com>
2022-11-01 23:08:55 +0800

    动态路由匹配

---
+++ gee/trie.go
@@ -0,0 +1,93 @@
+package gee
+
+import (
+	"fmt"
+	"strings"
+)
+
+//前缀树
+type node struct {
+	pattern  string  // 待匹配路由，例如 /p/:lang
+	part     string  // 路由中的一部分，例如 :lang
+	children []*node // 子节点，例如 [doc, tutorial, intro]
+	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
+}
+
+// 第一个匹配成功的节点，用于插入
+// 当字符与路由中的一部分相等或者遇到通配符时表示匹配成功，并返回该节点
+// 例如/hello 匹配/p/hello，或者hello匹配/p/*
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+// 所有匹配成功的节点，用于查找
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+//递归查找每一层的节点，如果没有匹配到当前part的节点，则新建一个
+func (n *node) insert(pattern string, parts []string, height int) {
+	//如果匹配到最后了，就新建一个节点
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	//当前路由正在匹配的节点
+	part := parts[height]
+	child := n.matchChild(part)
+	//如果匹配不到，说明当前的子节点没有可以匹配的，应该直接将其插入到当前节点下
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	//递归进入下一层
+	child.insert(pattern, parts, height+1)
+}
+
+//在前缀树中查找符合要求的字符串
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
+
+//
+func (n *node) travel(list *([]*node)) {
+	if n.pattern != "" {
+		*list = append(*list, n)
+	}
+	for _, child := range n.children {
+		child.travel(list)
+	}
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
